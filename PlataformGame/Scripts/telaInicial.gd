25fa1cf5eae9eca7fc55c64af7b6e067f62dc30d
Lucas Gabriel <lgabriel1607@gmail.com>
2023-06-21 12:13:15 -0300

    atualizações finais

--- PlataformGame/Scripts/telaInicial.gd
+++ PlataformGame/Scripts/telaInicial.gd
@@ -6,22 +6,16 @@ func _ready():
 	start_btn.grab_focus()
 	
 
+func _on_NovoJogo_pressed():
+	get_tree().change_scene("res://CenaNiveis/nivel1.tscn")
 
 func _on_Controles_pressed():
 	var telaControles = load("res://Cenas/telaControles.tscn").instance() #instanciação
 	get_tree().current_scene.add_child(telaControles) #coloca a imagem sobre a tela inicial
-	
-
 
 func _on_Sair_pressed():
 	get_tree().quit()
 
-
-func _on_Continuar_pressed():
-	var telaNivel = load("res://Cenas/selecionarNível.tscn").instance() #instanciação
-	get_tree().current_scene.add_child(telaNivel) #coloca a imagem sobre a tela inicial
-
-
-func _on_NovoJogo_pressed():
-	var primeiraTela = load("res://CenaNiveis/nivel1.tscn").instance() #instanciação
-	get_tree().current_scene.add_child(primeiraTela)
+func _on_Sobre_pressed():
+	var telaSobre = load("res://Cenas/telaSobre.tscn").instance() #instanciação
+	get_tree().current_scene.add_child(telaSobre)
---
+++ PlataformGame/Scripts/telaSobre.gd
@@ -0,0 +1,5 @@
+extends Control
+	
+
+func _on_btnVoltar_pressed():
+	queue_free()
