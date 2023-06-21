b283a3afa8a09d5a855059296a7e8ffaa23ed132
Silas Silva <silas@github.com>
2023-06-20 23:09:57 -0300

    correção

--- PlataformGame/Scripts/passarFase.gd
+++ PlataformGame/Scripts/passarFase.gd
@@ -10,5 +10,3 @@ func _on_passarFase_area_entered(body):
 	body.nivelAtual += 1
 	#print(body.nivelAtual)
 	get_tree().change_scene("res://CenaNiveis/nivel" + str(body.nivelAtual) + ".tscn")
-	body.nivelAtual += 1
-
